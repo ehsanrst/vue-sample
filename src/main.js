a3f6514e901c7ec766f0e80b21e44eb09a426095
Hossein Mobasher <hoseinmobasher@gmail.com>
2021-03-17 13:43:48 +0330

    Add primevue and samples to project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
-import { createApp } from 'vue'
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+
+import {createApp} from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config'
+
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+import Dialog from 'primevue/dialog'
+import Toast from 'primevue/toast'
+import ToastService from 'primevue/toastservice'
+
+const app = createApp(App)
+app.use(PrimeVue, { ripple: true })
+app.use(ToastService)
+
+app.component('Button', Button)
+app.component('InputText', InputText)
+app.component('Dialog', Dialog)
+app.component('Toast', Toast)
+
+app.mount('#app')
